c3c9d2a1c85bc51101a6c8af3bf1b4cf457a1ada
Sshashank5690 <sshubham.5690@gmail.com>
2024-05-09 08:52:04 +0530

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
-import Script from 'next/script';
-import './globals.css';
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
+import "./globals.css";
 // import Head from 'next/document';
 
 export default function RootLayout({ children }) {
